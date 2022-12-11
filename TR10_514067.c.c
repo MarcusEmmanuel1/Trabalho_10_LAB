c4df298cdf983826d4198d58b9cd1a0a030f62b5
MarcusEmmanuel1 <119619249+MarcusEmmanuel1@users.noreply.github.com>
2022-12-11 13:22:47 -0300

    Update TR10_514067.c.c

--- TR10_514067.c.c
+++ TR10_514067.c.c
@@ -6,34 +6,37 @@
 //MODELO DE EXEMPLO
 
 int main(){
-
+   
    	//Variaveis que podem ser usadas
    	unsigned int x[200];
    	int y = 0;
    	int i = 0;
-
+   	
    	//#########################
    	for(i=0;i<=200;i++)
         x[i]=0;
-
+      
     i = y = 0;
 	//limitacao de entrada//
 	printf("INSIRA QUALQUER NUMERO INTEIRO DE 0 A 5000 E -1 PARA PARAR: \n");
-
-	//verifica se o usuario digitou -1 pra parar ou um numero invalido//
+	
+	//Enquanto o usuatio nao digitar -1 pra parar, vai continuar armazenando numeros//	
     while (y != -1)
     {
     scanf("%i",&y);
+	
+	//checa se y nao eh igual a -1 ou maior que 5000//
+	if((y != -1) && (y <= 5000)){
 	//percorre o vetor armazenando os valores digitados em cada bit//
-	if(y != -1)
 	   x[y/32] = x[y/32] | (1<<(y%32));
 	}
-
+	}
+		
 	printf("OS NUMEROS EM ORDEM CRESCENTE: \n");
-
+    
     //percorre o vetor em ordem crescente e mostra os valores na tela//
     for(i=0;i<=200;i++)
-    {
+    {	
     for(y=0;y<32;y++)
     {
     if((x[i]&(1<<y)) != 0)
@@ -44,6 +47,6 @@ int main(){
     }
     }
    	//#########################
-
-	return 0;
+   
+	return 0;  
 }
